b2c532d80583baba6fdd03c31f2e9d0421f55337
Andrew Spiers <andrew@andrewspiers.net>
2015-07-23 17:31:38 +1000

    Change loadbalancer template logic
    Replace $loadbalancer_template var with $loadbalancer_image_id.
    
    Change-Id: Ie56355992e19b191bad3b1251d8ec9566a69e0b9

--- manifests/engine.pp
+++ manifests/engine.pp
@@ -124,6 +124,22 @@ class heat::engine (
     notify => Service['heat-engine'],
   }
 
+  if $::heat::loadbalancer_image_id {
+    file {'/etc/heat/environment.d/lb.yaml':
+      owner   => heat,
+      group   => heat,
+      content => template('heat/lb.yaml.erb'),
+      notify  => Service['heat-engine'],
+    }
+  } else {
+    file {'/etc/heat/environment.d/lb.yaml':
+      ensure  => absent,
+      notify  => Service['heat-engine'],
+    }
+  }
+
+
+
   nagios::nrpe::service {
     'service_heat_engine':
       check_command => "/usr/lib/nagios/plugins/check_procs -c ${procs}:${procs} -u heat -a /usr/bin/heat-engine";
